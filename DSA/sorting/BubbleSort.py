047367de5c83bc4a349bcbe131c2f9b304b9a495
Atharva_404 <deshmukhsambhajirao9@gmail.com>
2024-01-17 13:06:53 +0530

    Add counter to bubbleSort function and update notes

--- DSA/sorting/BubbleSort.py
+++ DSA/sorting/BubbleSort.py
@@ -1,9 +1,16 @@
 def bubbleSort(ls):
+    counter=0
     for i in range(len(ls)-1):
+        counter+=1
         for j in range(len(ls)-1-i):
-            if ls[j]>ls[j+1]:
+            if len(ls[j])>len(ls[j+1]):
                 ls[j],ls[j+1]=ls[j+1],ls[j]
+    print(counter)
 
-ls=list(map(int,input().split()))
-bubbleSort(ls)
-print(*ls)
+# ls=list(map(int,input().split()))
+# bubbleSort(ls)
+# print(*ls)
+
+la=["abc","a","hello"]
+bubbleSort(la)
+print(*la)
