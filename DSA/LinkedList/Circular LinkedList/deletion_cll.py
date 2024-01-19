03cf00203eeda9a9ca0cc7377b446232ddee85a9
Atharva_404 <deshmukhsambhajirao9@gmail.com>
2024-01-19 11:23:57 +0530

    Add method to insert node at a specific position

--- DSA/LinkedList/Circular LinkedList/deletion_cll.py
+++ DSA/LinkedList/Circular LinkedList/deletion_cll.py
@@ -4,13 +4,16 @@ class  Node :
         self.next=None
         
 class CircularLL:
-    def __init__(self):
+    def __init__(self,size):
         self.head=None
         self.tail=None
+        self.size=size
+        
         
     def create(self):
-        f=1
-        while(f==1):
+        f=self.size
+        print("You can enter ",f," nodes")
+        while(f>0):
             d=int(input("enter the number to add in linked list: "))
             n=Node(d)
             if(self.head is None):
@@ -21,7 +24,7 @@ class CircularLL:
                 self.tail.next=n
                 n.next=self.head
                 self.tail=n
-            f=int(input("enter the value 1 to take input again: "))
+            f-=1
             
     def reverse(self):
         prev,curr,nxt=self.tail,None,self.head
@@ -53,12 +56,14 @@ class CircularLL:
         n.next=self.head
         self.head=n
         self.tail.next=self.head
+        self.size+=1
     def addAtEnd(self):
         d=int(input("enter the number to add at end: "))
         n=Node(d)
         self.tail.next=n
         n.next=self.head
-        self.tail=n                     
+        self.tail=n
+        self.size+=1                   
     def addAtPosition(self):
         d=int(input("enter the number to add at position: "))
         n=Node(d)
@@ -69,12 +74,44 @@ class CircularLL:
         n.next=temp.next
         temp.next=n
         self.tail.next=self.head
-        
-        
+        self.size+=1
+    def del_beg(self):
+        if(self.head is None):
+            print("List is empty")
+        else:
+            temp=self.head
+            self.head=temp.next
+            self.tail.next=self.head
+            self.size-=1
+    def del_last(self):
+        temp=self.head
+        if(self.head is None):
+            print("List is empty")
+        else:
+            while(temp.next!=self.tail):
+                temp=temp.next
+            temp.next=self.head
+            self.tail=temp
+            self.size-=1
+    def del_at_pos(self):
+        pos=int(input("Enter the position to be deleted: "))
+        prev=self.head
+        temp=prev.next
+        c=0
+        while(c<pos-2):
+            temp=temp.next
+            prev=prev.next
+            c+=1
+            if(temp.next==self.head):
+                print("Position out of range")
+                break
+        prev.next=temp.next
+        del temp
+        self.size-=1
+                 
 # n1=Node(10)
-l1=CircularLL()
+l1=CircularLL(5)
 # l1.head=n1
-
 # n2=Node(20)
 # n1.next=n2
 # n3=Node(30)
@@ -84,9 +121,12 @@ l1=CircularLL()
 # l1.tail=n4
 # l1.tail.next=l1.head
 l1.create()
-l1.addAtBeg()
-l1.addAtEnd()
-l1.display()
-l1.addAtPosition()
-l1.reverse()
+# l1.addAtBeg()
+# l1.addAtEnd()
+# l1.display()
+# l1.addAtPosition()
+# l1.reverse()
+# l1.del_beg()
+# l1.del_last()
+l1.del_at_pos()
 l1.display()
